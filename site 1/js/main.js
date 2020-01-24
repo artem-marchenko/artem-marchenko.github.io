c564fa1577d36cb4fce7dfd0d1064c6f21dd4bbc
artem-marchenko <artemmarchenko989@gmail.com>
2020-01-24 13:23:29 +0300

    -

---
+++ site 1/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+	/* Портфолио */
+	$('.button[filter]').click(function () {
+	   if ($(this).attr('val') == 'off') {
+		  $('.button[filter]').attr('val', 'off').removeClass('focused');
+		  $(this).attr('val', 'on').addClass('focused');
+		  $('.filter > div').hide(300);
+		  $('.filter > div[filter=' + $(this).attr('filter') + ']').show(300);
+		  if ($(this).attr('filter') == 'all') {
+			 $('.button[filter]').attr('val', 'off').removeClass('focused');
+			 $(this).attr('val', 'on').addClass('focused');
+			 $('.filter > div').show(300);
+		  }
+	   }
+	});
+	/* Слайдер */
+	$('.multiple-items').slick({
+	   infinite: true,
+	   slidesToShow: 3,
+	   slidesToScroll: 3,
+	   arrows: false,
+	   dots: true,
+	   dotsClass: 'dots-style',
+	   responsive: [{
+		  breakpoint: 1025,
+		  settings: {
+			 slidesToShow: 2,
+			 slidesToScroll: 2,
+		  }
+	   }, {
+		  breakpoint: 480,
+		  settings: {
+			 slidesToShow: 1,
+			 slidesToScroll: 1,
+		  }
+	   }]
+	});
+	/* Меню */
+	$('ul.menu a[href^="#"').click(function () {
+	   var target = $(this).attr('href');
+	   $('html, body').animate({
+		  scrollTop: $(target).offset().top
+	   }, 500);
+	   $('ul.menu a[href^="#"').css({
+		  'color': '#212121'
+	   });
+	   $(this).css({
+		  'color': '#004bee'
+	   });
+	   return false;
+	});
+});
